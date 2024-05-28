d05fcea3f305cee916984b0ea830650324c43edb
manjuvipin <manjusathyan9656@gmail.com>
2024-05-28 11:02:59 +0530

    j

---
+++ Project/3tierApp/3tierApp/CatagoryView.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BLL.User.Manager;
+
+namespace _3tierApp
+{
+    public partial class CatagoryView : System.Web.UI.Page
+    {
+        CatagoryManager catagory = new CatagoryManager();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack == false)
+            {
+                ListAllCategories();
+            }
+        }
+        public void ListAllCategories()
+        {
+            DataList1.DataSource = catagory.SellectAllCategory();
+            DataList1.DataBind();
+        }
+    }
+}
